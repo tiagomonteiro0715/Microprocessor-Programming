778b1a78a7442debb4e49349c45b92a48cb1a84c
Tiago monteiro <tiago.monteiro0715@gmail.com>
2021-10-21 19:29:01 +0100

    Add files via upload

--- second-work/final-work/main.c
+++ second-work/final-work/main.c
@@ -2,54 +2,45 @@
 #include <stdlib.h>
 /*
 deve apresentar no msm formato
-programa lida com o maximo 1000 dias == 86400000 segundos
+programa lida com o maximo 1000 dias == 86 400 000 segundos
 */
 /*
 depois de estar feito - tratar melhor das variaveis + melhor nome de variaveis
+ver o que fizeram nos exerciceos propostos
 */
 int main()
 {
 
-        int valday1=0, valhour1=0, valmin1=0, valsec1=0 ;
-        int valday2=0, valhour2=0, valmin2=0, valsec2=0 ;
-        int valday=0, valhour=0, valmin=0, valsec=0 ;
+        int valhour1=0, valmin1=0, valsec1=0 ;
+        int valhour2=0, valmin2=0, valsec2=0 ;
+        short int valday=0, valday1=0, valday2=0;
+        int valhour=0, valmin=0, valsec=0 ;
 
 
         printf("Introduza o tempo no.1 (dia)d (hora)h (minuto)m (segundo)s ");
         printf("\n");
-        scanf(" %d %d %d %d", &valday1, &valhour1, &valmin1, &valsec1);
+        scanf(" %hd %d %d %d", &valday1, &valhour1, &valmin1, &valsec1);
         printf("\n");
-        printf(" %d d %d h %d m %d s ", valday1, valhour1, valmin1, valsec1);
+        printf(" %hd d %d h %d m %d s ", valday1, valhour1, valmin1, valsec1);
         printf("\n");
         printf("Introduza o tempo no.1 (dia)d (hora)h (minuto)m (segundo)s \n");
-        scanf(" %d %d %d %d", &valday2, &valhour2, &valmin2, &valsec2);
+        scanf(" %hd %d %d %d", &valday2, &valhour2, &valmin2, &valsec2);
+
+
 
         int totalSec1 = valsec1 + (60 * valmin1) + (3600 * valhour1) + (86400 * valday1);
         int totalSec2 = valsec2 + (60 * valmin2) + (3600 * valhour2) + (86400 * valday2);
-        int totalSec = totalSec2 - totalSec1;
+        unsigned long int totalSec = totalSec2 - totalSec1;
 
-        printf("%d - %d = %d", totalSec2, totalSec1, totalSec);
+        printf("%d - %d = %d \n", totalSec2, totalSec1, totalSec);
 
-        //find days
-        valday = (float) totalSec / 86400;
-        int daysRounded = (int) valday;
-        float partday = valday - daysRounded;
 
-        //find hours
-        valhour = (float) partday * 24;
-        int hoursRounded = (int) valhour;
-        float parthour = valhour - hoursRounded;
+        valday = totalSec /(24*60*60);
+        valhour = valday % 3600;
+        valmin = totalSec % (3600 / 60);
+        valsec = totalSec % (60);
 
-        //find minutes
-        valmin = (float) parthour * 60;
-        int minRounded = (int) valmin;
-        float partmin = valmin - minRounded;
-
-        //find seconds
-        valsec = (float) partmin * 60;
-        valsec = (int) valsec;
-
-        printf("A diferença entre os dois tempos é de %d segundos = %d d %d h %d m %d s",
+        printf("A diferenca entre os dois tempos e de %u segundos = %hd d %d h %d m %d s",
         totalSec,
         valday,
         valhour,
