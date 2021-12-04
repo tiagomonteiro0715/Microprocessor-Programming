72a57c4acf62409889014451bc7fdf40acbe15da
Tiago monteiro <tiago.monteiro0715@gmail.com>
2021-12-04 16:55:12 +0000

    Add files via upload

---
+++ seventy-work/final/finalProject.c
@@ -0,0 +1,41 @@
+/*
+adicionar: função que adiciona e retiraum valor inteiro a uma variavel que recebe de entrada
+*/
+#include <stdio.h>
+#include <string.h>
+#define bool int
+#define TRUE 1
+#define FALSE 0
+
+#define MAX_WORD_LENGTH 21
+#define MAX_TEXT_LENGTH 81
+#define MAX_WORD 20
+
+void add(int *person, int amount) {
+  printf("%d", *person + amount);
+
+}
+void main() {
+    int joao, ana, mariana, catarina,
+    pedro, filipa, margarida, jose,
+    jorge, susana, cristina;
+
+    joao = 1000;
+    ana = 2500;
+    mariana = 3000;
+    catarina = 1200;
+    pedro = 2500;
+    filipa = 340;
+    margarida = 3400;
+    jose = 120;
+    jorge = 340;
+    susana = 120;
+    cristina = 1290;
+
+    int *toAdd;
+    short int storeVal;   // para conter a escolha do utilizador.
+
+    add(&joao, 20);
+
+
+}
--- seventy-work/frases.c
+++ seventy-work/frases.c
@@ -10,8 +10,8 @@
 #define MAX_WORD_LENGTH 21
 #define MAX_TEXT_LENGTH 81
 #define MAX_WORD 20
-      //código aqui
-main() {
+
+int main() {
     char frase1 [MAX_TEXT_LENGTH] = "A string that contains a dog and a cat";
     char frase2 [MAX_TEXT_LENGTH] = "A string that contains camel and mouse";
     char frase3 [MAX_TEXT_LENGTH] = "The dog was eating with the horse";
@@ -28,24 +28,52 @@ main() {
     char *frase_ptr [MAX_WORD];                     // para apontar a frases
     char *pal_ptr [MAX_WORD];                      // para apontar a palavras
     char ** frase, ** palavra;
-    int num_frase, num_palavra;             // para conter a escolha do util.
+    int num_frase, num_palavra;             // para conter a escolha do utilizador.
 
-    frase_ptr [0] = frase1;
-    frase_ptr [1] = frase2;
-    frase_ptr [2] = frase3;
-    frase_ptr [3] = frase4;
-    frase_ptr [4] = frase5;
-    frase_ptr [5] = frase6;
-    frase_ptr [6] = frase7;
-    pal_ptr [0] = palavra1;
-    pal_ptr [1] = palavra2;
-    pal_ptr [2] = palavra3;
-    pal_ptr [3] = palavra4;
-    pal_ptr [4] = palavra5;
-    pal_ptr [5] = palavra6;
+    frase_ptr[0] = frase1;
+    frase_ptr[1] = frase2;
+    frase_ptr[2] = frase3;
+    frase_ptr[3] = frase4;
+    frase_ptr[4] = frase5;
+    frase_ptr[5] = frase6;
+    frase_ptr[6] = frase7;
+    pal_ptr[0] = palavra1;
+    pal_ptr[1] = palavra2;
+    pal_ptr[2] = palavra3;
+    pal_ptr[3] = palavra4;
+    pal_ptr[4] = palavra5;
+    pal_ptr[5] = palavra6;
     while (1) {
-        escolha (&num_frase, &num_palavra);
-        //código_aqui
-        ver_se_contem (frase_actual, palavra_actual);
+
+
+      int *frase_actual;
+      int *palavra_actual;
+
+      printf("\nEscolha um numero da frase:");
+      scanf("%d", &num_frase);
+
+      printf("\nEscolha o numero da palavra:");
+      scanf("%d",  &num_palavra);
+
+      frase_actual = &num_frase;
+      palavra_actual = &num_palavra;
+
+      printf("\n Numero de frase escolhido: %d \nNumero de palavra escolhido: %d \n", *frase_actual, *palavra_actual);
+
+      printf("\nPalavra escolhida:%s", frase_ptr[*frase_actual]);
+      printf("\nPalavra escolhida:%s\n", pal_ptr[*palavra_actual]);
+
+
+const char *sentence = frase_ptr[*frase_actual];
+const char *wordInput = pal_ptr[*palavra_actual];
+
+const char * isThere = strstr(sentence, wordInput);
+if (isThere == NULL){
+  printf("\nA frase nao contem a palavra\n");
+} else{
+  printf("A frase contem a palavra %s", wordInput);
+}
+
+
     }
 }
